31659457e12436522aa35a3e223d55ef90245e49
Guilherme <guilherme.vasconcelos61@aluno.ifce.edu.br>
2025-05-31 12:02:38 -0300

    Adição da classe Perfil

---
+++ perfil_do_usuario.py
@@ -0,0 +1,6 @@
+class Perfil:
+    def __init__(self, nome, email, foto, tipo_perfil):
+        self.nome_completo = nome
+        self.email = email
+        self.foto = foto
+        self.tipo_perfil = tipo_perfil
